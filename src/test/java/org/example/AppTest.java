560809f83fa4e7a490134c8e402b842d15ff39f4
ezgikucuk <72459592+ezgikucuk@users.noreply.github.com>
2021-02-15 02:12:09 +0300

    Comment lines added

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -9,13 +9,17 @@ public class AppTest {
     @Test
     void test_search_api() {
         OMDBSearchApi api = new OMDBSearchApi("24155add");
+        //Querying Harry Potter movies on Omdb search api
         Response queryResponse = api.querySearch("Harry Potter");
+        //Taking imdb id from response to filter by id
         String id = queryResponse.jsonPath().getString("Search.find {it.Title == 'Harry Potter and the Sorcerer\\'s Stone'}.imdbID");
 
         Response idResponse = api.idSearch(id);
+        //Validating response fields
         Assert.assertNotNull(idResponse.jsonPath().getString("Title"));
         Assert.assertNotNull(idResponse.jsonPath().getString("Year"));
         Assert.assertNotNull(idResponse.jsonPath().getString("Released"));
+        //Checking response status code
         Assert.assertEquals(idResponse.statusCode(),200);
     }
 
